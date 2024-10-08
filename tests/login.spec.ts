aec5f953c79e8daa329489df2ba3507e274adc39
Aditya Gidh <aagidh@us.ibm.com>
2024-10-08 15:46:02 -0400

    some refactoring for basic tests
    Signed-off-by: Aditya Gidh <aagidh@us.ibm.com>

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,32 +1,24 @@
-import { test, expect, chromium } from '@playwright/test';
-import path from 'path';
+import { test, expect } from '@playwright/test';
 
-const pageURL = 'https://ui.instructlab.ai/login';
-
-const authFile = path.join(__dirname, '../playwright/.auth/user.json');
+test.beforeEach(async ({ page }) => {
+  await page.goto('https://ui.instructlab.ai/');
+});
 
 test('has title', async ({ page }) => {
-  await page.goto(pageURL);
   await expect(page).toHaveTitle(/InstructLab UI/);
 });
 
 test('corresponding anchor links', async ({ page }) => {
-  await page.goto(pageURL);
-
   const loginButton = page.getByRole('button');
   await expect(loginButton).toContainText('Sign in with GitHub');
 });
 
 test('corresponding text content', async ({ page }) => {
-  await page.goto(pageURL);
-
   const loginButton = page.getByRole('button');
   await expect(loginButton).toContainText('Sign in with GitHub');
 });
 
 test('all the call to actions links exists on the landing page', async ({ page }) => {
-  await page.goto(pageURL);
-
   await expect(page.getByRole('link', { name: 'GitHub' })).toBeVisible();
 
   await expect(page.getByRole('link', { name: 'Collaborate' })).toBeVisible();
