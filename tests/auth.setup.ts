e65fcdf6106964e8cecdea7c0618bb454e5cf475
Aditya Gidh <aagidh@us.ibm.com>
2024-10-08 15:46:02 -0400

    some refactoring for basic tests
    Signed-off-by: Aditya Gidh <aagidh@us.ibm.com>

--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -1,30 +1,25 @@
-import { test as setup, chromium } from '@playwright/test';
+import { test as setup, chromium, Page, Browser } from '@playwright/test';
 import path from 'path';
 
 const authFile = path.join(__dirname, '../playwright/.auth/user.json');
 
 setup('should authenticate on ui.instructlab.ai', async () => {
-  const browser = await chromium.launch({ headless: false }); // Set headless to true for headless testing
-  const page = await browser.newPage();
-
-  // Replace with your actual credentials
-  const username = process.env.USERNAME!;
-  const password = process.env.PASSWORD!;
+  const browser: Browser = await chromium.launch({ headless: false }); // Set headless to true for headless testing
+  const context = await browser.newContext();
+  const page: Page = await browser.newPage();
 
   await page.goto('https://ui.instructlab.ai/login');
 
+  await page.getByRole('button', { name: 'Sign in with GitHub' }).click();
   // Locate the username and password input fields
-  const usernameInput = page.locator('[name="login"]');
-  const passwordInput = page.locator('[name="password"]');
   const loginButton = page.locator('[type="submit"]');
 
   // Enter the username and password
-  await usernameInput.fill(username);
-  await passwordInput.fill(password);
+  await page.getByLabel('Username or email address').fill(process.env.USERNAME!);
+  await page.getByLabel('Password').fill(process.env.PASSWORD!);
 
   // Submit the login form
   await loginButton.click();
-  await page.getByLabel('user menu dropdown'); 
   await page.context().storageState({ path: authFile });
 
   await browser.close();
