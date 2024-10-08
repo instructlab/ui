c5289ef1ef0f058764c4c658ed94d855b535edfc
Aditya Gidh <aagidh@us.ibm.com>
2024-10-08 16:22:31 -0400

    basic playwright setup
    Signed-off-by: Aditya Gidh <aagidh@us.ibm.com>

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -4,21 +4,21 @@ test.beforeEach(async ({ page }) => {
   await page.goto('https://ui.instructlab.ai/');
 });
 
-test('has title', async ({ page }) => {
+test('Login Page has title', async ({ page }) => {
   await expect(page).toHaveTitle(/InstructLab UI/);
 });
 
-test('corresponding anchor links', async ({ page }) => {
+test('Login Page has corresponding anchor links', async ({ page }) => {
   const loginButton = page.getByRole('button');
   await expect(loginButton).toContainText('Sign in with GitHub');
 });
 
-test('corresponding text content', async ({ page }) => {
+test('Login Page has corresponding text content', async ({ page }) => {
   const loginButton = page.getByRole('button');
   await expect(loginButton).toContainText('Sign in with GitHub');
 });
 
-test('all the call to actions links exists on the landing page', async ({ page }) => {
+test('Login Page has all the call to actions links exists on the landing page', async ({ page }) => {
   await expect(page.getByRole('link', { name: 'GitHub' })).toBeVisible();
 
   await expect(page.getByRole('link', { name: 'Collaborate' })).toBeVisible();
