f3d4e4429f0ff9cf801be653e3a0db9030060a4b
Brent Salisbury <bsalisbu@redhat.com>
2024-10-16 14:31:26 -0400

    Add the ilab-public org signup link to the login page
    Signed-off-by: Brent Salisbury <bsalisbu@redhat.com>

--- src/app/login/githublogin.tsx
+++ src/app/login/githublogin.tsx
@@ -8,9 +8,23 @@ import GithubIcon from '@patternfly/react-icons/dist/dynamic/icons/github-icon';
 import './githublogin.css';
 import { signIn } from 'next-auth/react';
 
+const CustomLogoIcon: React.FC = () => <img src="/updated-logo.png" alt="Sign up logo" style={{ width: '15px', height: '15px' }} />;
+
 const GithubLogin: React.FC = () => {
   const handleGitHubLogin = () => {
     signIn('github', { callbackUrl: '/' }); // Redirect to home page after login
+  };
+
+  const handleSignUpClick = (event: React.MouseEvent) => {
+    const signupUrl =
+      'https://instructlab-inviter-instructlab-public-inviter.qa-ui-instructlab-ai-0e3e0ef4c9c6d831e8aa6fe01f33bfc4-0000.us-south.containers.appdomain.cloud/'; // Replace with the actual signup URL
+    if (event.shiftKey || event.ctrlKey || event.metaKey) {
+      // Open in a new tab if Shift, Ctrl, or Cmd is pressed
+      window.open(signupUrl, '_blank');
+    } else {
+      // Otherwise, navigate in the current window
+      window.location.href = signupUrl;
+    }
   };
 
   return (
@@ -23,7 +37,8 @@ const GithubLogin: React.FC = () => {
             </TextContent>
             <TextContent>
               <Text className="description-text">
-                Join the novel, community based movement to <br></br>create truly open source LLMs
+                Join the novel, community based movement to <br />
+                create truly open source LLMs
               </Text>
             </TextContent>
             <div className="login-container">
@@ -32,10 +47,20 @@ const GithubLogin: React.FC = () => {
                 icon={<GithubIcon />}
                 iconPosition="left"
                 size="lg"
-                style={{ backgroundColor: 'black' }}
+                style={{ backgroundColor: 'black', marginRight: '20px' }}
                 onClick={handleGitHubLogin}
               >
                 Sign in with GitHub
+              </Button>
+              <Button
+                variant="primary"
+                icon={<CustomLogoIcon />}
+                iconPosition="left"
+                size="lg"
+                style={{ backgroundColor: 'black' }}
+                onClick={handleSignUpClick}
+              >
+                Sign up
               </Button>
             </div>
             <TextContent>
