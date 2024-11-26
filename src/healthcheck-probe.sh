0d2fa41e9f4c80c7984b18a8088c712acc14a5c4
greg pereira <grpereir@redhat.com>
2024-11-26 06:37:29 -0800

    moving healthcheck-sidecar dir to root and update pathrefs
    Signed-off-by: greg pereira <grpereir@redhat.com>

--- src/healthcheck-probe.sh
+++ src/healthcheck-probe.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+# -*- indent-tabs-mode: nil; tab-width: 2; sh-indentation: 2; -*-
+
+# probe script to check to run as readinesProb (https://kubernetes.io/docs/tasks/configure-pod-container/configure-liveness-readiness-startup-probes/#define-readiness-probes)
 # requires jq
+
+set -x
+set -e
+set -o pipefail
 
 health_curl=$(curl localhost:8080/health)
 
