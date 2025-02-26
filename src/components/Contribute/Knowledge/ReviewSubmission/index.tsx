04b491b5c19c85853716744f35aa940d78666374
Aditya Gidh <aagidh@us.ibm.com>
2025-02-26 17:14:43 -0500

    523: UI updates for the submission steps on "Skill" & "Knowledge" Review forms (#581)
    * updates for the knowledge review form
    * adopt vars instead of absolute values
    * adopt patternfly content component
    
    Signed-off-by: Aditya Gidh <aagidh@us.ibm.com>

--- src/components/Contribute/Knowledge/ReviewSubmission/index.tsx
+++ src/components/Contribute/Knowledge/ReviewSubmission/index.tsx
@@ -1,7 +1,8 @@
 // src/components/Contribute/Knowledge/ReviewSubmission/ReviewSubmission.tsx
 import { KnowledgeFormData } from '@/types';
-import { Content, ContentVariants, Form } from '@patternfly/react-core';
+import { Content, ContentVariants, Form, Accordion, AccordionContent, AccordionItem, AccordionToggle } from '@patternfly/react-core';
 import React from 'react';
+import '../../Skill/ReviewSubmission/submission.css';
 
 interface ReviewSubmissionProps {
   knowledgeFormData: KnowledgeFormData;
@@ -9,107 +10,168 @@ interface ReviewSubmissionProps {
 }
 
 export const ReviewSubmission: React.FC<ReviewSubmissionProps> = ({ knowledgeFormData, isGithubMode }) => {
+  const [expanded, setExpanded] = React.useState<{ [key: string]: boolean }>({});
+
+  const onToggle = (id: number) => {
+    setExpanded((prevState) => ({
+      ...prevState,
+      [id]: !prevState[id]
+    }));
+  };
+
   return (
-    <Form>
-      <Content component={ContentVariants.h2}>Review Submission</Content>
-      {/* Author Information */}
-      <p>
-        <strong>Author Information</strong>
-      </p>
-      <p>
-        <i>Name:</i> {knowledgeFormData.name}
-      </p>
-      <p>
-        <i>Email:</i> {knowledgeFormData.email}
-      </p>
-
-      {/* Knowledge Information */}
-      <h3>Knowledge Information</h3>
-      <p>
-        <strong>Knowledge Information</strong>
-      </p>
-      <p>
-        <i>Submission Summary:</i> {knowledgeFormData.submissionSummary}
-      </p>
-      <p>
-        <i>Domain:</i> {knowledgeFormData.domain}
-      </p>
-      <p>
-        <i>Document Outline:</i> {knowledgeFormData.documentOutline}
-      </p>
-
-      {/* File Path Information */}
-      <p>
-        <strong>File Path Information</strong>
-      </p>
-
-      <p>
-        <i>File Path:</i> {knowledgeFormData.filePath}
-      </p>
-
-      {/* Seed Examples */}
-      <p>
-        <strong>Seed Examples</strong>
-      </p>
-
-      {knowledgeFormData.seedExamples.map((seedExample, index) => (
-        <div key={index}>
-          <strong>Seed Example {index + 1}</strong>
-          <p>
-            <i>Context:</i> {seedExample.context}
-          </p>
-          {seedExample.questionAndAnswers.map((qa, qaIndex) => (
-            <div key={qaIndex}>
-              <p>
-                <i>Question {qaIndex + 1}:</i> {qa.question}
-              </p>
-              <p>
-                <i>Answer {qaIndex + 1}:</i> {qa.answer}
-              </p>
+    <>
+      <Form>
+        <section className="review-submission-container">
+          <Content component={ContentVariants.h2}>Review</Content>
+          <Content component={ContentVariants.p}>
+            Review the information below and click finish to submit your skill contribution. Use the back button to make changes.
+          </Content>
+          {/* Author Information */}
+          <article>
+            <div className="info-wrapper">
+              <Content component={ContentVariants.p} className="submission-titles">
+                Contributor Information
+              </Content>
+              <Content component={ContentVariants.p} className="submission-subtitles">
+                Information required for a Github Developer Certificate of Origin (DCO) sign-off.
+              </Content>
             </div>
-          ))}
-        </div>
-      ))}
 
-      {/* Document Information */}
-      <p>
-        <strong>Document Information</strong>
-      </p>
+            <div className="contributors-wrapper">
+              <Content component={ContentVariants.h5} className="category-titles">
+                Contributors
+              </Content>
+              <Content component={ContentVariants.p}>{knowledgeFormData.name}</Content>
+              <Content component={ContentVariants.p}>{knowledgeFormData.email}</Content>
+            </div>
+          </article>
 
-      <p>
-        <i>Repository URL:</i> {knowledgeFormData.knowledgeDocumentRepositoryUrl}
-      </p>
-      <p>
-        <i>Commit:</i> {knowledgeFormData.knowledgeDocumentCommit}
-      </p>
-      <p>
-        <i>Document Name:</i> {knowledgeFormData.documentName}
-      </p>
+          {/* Knowledge Information */}
+          <article>
+            <div className="info-wrapper">
+              <Content component={ContentVariants.p} className="submission-titles">
+                Knowledge Information
+              </Content>
+              <Content component={ContentVariants.p} className="submission-subtitles">
+                Brief information about the Knowledge and the directory path for the QnA and Attribution files.
+              </Content>
+            </div>
 
-      {/* Attribution Information */}
-      {isGithubMode && (
-        <div>
-          <p>
-            <strong>Attribution Information</strong>
-          </p>
-          <p>
-            <i>Title of Work:</i> {knowledgeFormData.titleWork}
-          </p>
-          <p>
-            <i>Link to Work:</i> {knowledgeFormData.linkWork}
-          </p>
-          <p>
-            <i>Revision:</i> {knowledgeFormData.revision}
-          </p>
-          <p>
-            <i>License of Work:</i> {knowledgeFormData.licenseWork}
-          </p>
-          <p>
-            <i>Creators:</i> {knowledgeFormData.creators}
-          </p>
-        </div>
-      )}
-    </Form>
+            <Content component={ContentVariants.h5} className="category-titles">
+              Submission summary
+            </Content>
+            <Content component={ContentVariants.p}>{knowledgeFormData.submissionSummary}</Content>
+            <Content component={ContentVariants.h5} className="category-titles">
+              Domain
+            </Content>
+            <Content component={ContentVariants.p}>{knowledgeFormData.domain}</Content>
+            <Content component={ContentVariants.h5} className="category-titles">
+              Document Outline
+            </Content>
+            <Content component={ContentVariants.p}>{knowledgeFormData.documentOutline}</Content>
+          </article>
+
+          <article>
+            {/* Directory path */}
+            <Content component={ContentVariants.h5} className="category-titles">
+              Directory path
+            </Content>
+
+            <Content component={ContentVariants.p}>{knowledgeFormData.filePath}</Content>
+          </article>
+
+          {/* Seed Examples */}
+          <article>
+            <div className="info-wrapper">
+              <Content component={ContentVariants.p}>Seed Examples</Content>
+              <Content component={ContentVariants.p} className="submission-subtitles">
+                Data that will be used to start teaching your model.
+              </Content>
+            </div>
+
+            {knowledgeFormData.seedExamples?.map((seedExample, index) => (
+              <Accordion asDefinitionList={false} className="accordion-wrapper" key={`seed-${index}`}>
+                <AccordionItem isExpanded={!!expanded[index]} key={`accordion-item-${index}`}>
+                  <AccordionToggle onClick={() => onToggle(index)} id={`seed-example-toggle-${index}`} className="accordion-toggle-item">
+                    Sample {index + 1}
+                  </AccordionToggle>
+                  <AccordionContent id={`seed-example-content-${index}`}>
+                    <div className="accordion-content">
+                      <Content component={ContentVariants.h5} className="seed-category-titles">
+                        Context:
+                      </Content>{' '}
+                      {seedExample.context}
+                    </div>
+                    {seedExample.questionAndAnswers.map((qa, qaIndex) => (
+                      <React.Fragment key={`qa-${index}-${qaIndex}`}>
+                        <div className="accordion-content">
+                          <Content component={ContentVariants.h5} className="seed-category-titles">
+                            Question:
+                          </Content>{' '}
+                          {qa.question}
+                        </div>
+                        <div className="accordion-content">
+                          <Content component={ContentVariants.h5} className="seed-category-titles">
+                            Answer:
+                          </Content>{' '}
+                          {qa.answer}
+                        </div>
+                      </React.Fragment>
+                    ))}
+                  </AccordionContent>
+                </AccordionItem>
+              </Accordion>
+            ))}
+          </article>
+
+          {/* Document Information */}
+          <article>
+            <Content component={ContentVariants.p} className="category-titles">
+              Document Information
+            </Content>
+            <Content component={ContentVariants.h5} className="category-titles">
+              Repository URL
+            </Content>
+            <Content component={ContentVariants.p}>{knowledgeFormData.knowledgeDocumentRepositoryUrl}</Content>
+            <Content component={ContentVariants.h5} className="category-titles">
+              Commit
+            </Content>
+            <Content component={ContentVariants.p}>{knowledgeFormData.knowledgeDocumentCommit}</Content>
+            <Content component={ContentVariants.h5} className="category-titles">
+              Name
+            </Content>
+            <Content component={ContentVariants.p}>{knowledgeFormData.documentName}</Content>
+          </article>
+
+          <article className="info-wrapper">
+            {/* Attribution Information */}
+            {isGithubMode && (
+              <div>
+                <Content component={ContentVariants.p}>
+                  <strong>Attribution Information</strong>
+                </Content>
+                <Content component={ContentVariants.p}>
+                  <i>Title of Work:</i> {knowledgeFormData.titleWork}
+                </Content>
+                <Content component={ContentVariants.p}>
+                  <i>Link to Work:</i> {knowledgeFormData.linkWork}
+                </Content>
+                <Content component={ContentVariants.p}>
+                  <i>Revision:</i> {knowledgeFormData.revision}
+                </Content>
+                <Content component={ContentVariants.p}>
+                  <i>License of Work:</i> {knowledgeFormData.licenseWork}
+                </Content>
+                <Content component={ContentVariants.p}>
+                  <i>Creators:</i> {knowledgeFormData.creators}
+                </Content>
+              </div>
+            )}
+          </article>
+        </section>
+      </Form>
+    </>
   );
 };
 
--- src/components/Contribute/Skill/ReviewSubmission/index.tsx
+++ src/components/Contribute/Skill/ReviewSubmission/index.tsx
@@ -1,7 +1,8 @@
 // src/components/Contribute/Skill/ReviewSubmission/ReviewSubmission.tsx
 import { SkillFormData } from '@/types';
-import { Content, ContentVariants } from '@patternfly/react-core';
 import React from 'react';
+import './submission.css';
+import { Content, ContentVariants, Accordion, AccordionContent, AccordionItem, AccordionToggle } from '@patternfly/react-core';
 
 interface ReviewSubmissionProps {
   skillFormData: SkillFormData;
@@ -9,78 +10,125 @@ interface ReviewSubmissionProps {
 }
 
 export const ReviewSubmission: React.FC<ReviewSubmissionProps> = ({ skillFormData, isGithubMode }) => {
+  const [expanded, setExpanded] = React.useState<{ [key: string]: boolean }>({});
+
+  const onToggle = (id: number) => {
+    setExpanded((prevState) => ({
+      ...prevState,
+      [id]: !prevState[id]
+    }));
+  };
+
   return (
-    <div>
-      <Content component={ContentVariants.h2}>Review Submission</Content>
-      {/* Author Information */}
-      <p>
-        <strong>Author Information</strong>
-      </p>
-      <p>
-        <i>Name:</i> {skillFormData.name}
-      </p>
-      <p>
-        <i>Email:</i> {skillFormData.email}
-      </p>
+    <>
+      <section className="review-submission-container">
+        <Content component={ContentVariants.h3}>Review</Content>
+        <Content component={ContentVariants.p}>
+          Review the information below and click finish to submit your skill contribution. Use the back button to make changes.
+        </Content>
+        {/* Author Information */}
+        <article>
+          <div className="info-wrapper">
+            <Content component={ContentVariants.p} className="submission-titles">
+              Contributor Information
+            </Content>
+            <Content component={ContentVariants.p} className="submission-subtitles">
+              Information required for a Github Developer Certificate of Origin (DCO) sign-off.
+            </Content>
 
-      {/* Skill Information */}
-      <p>
-        <strong>Skill Information</strong>
-      </p>
-      <p>
-        <i>Submission Summary:</i> {skillFormData.submissionSummary}
-      </p>
-      <p>
-        <i>Document Outline:</i> {skillFormData.documentOutline}
-      </p>
+            <div className="contributors-wrapper">
+              <Content component={ContentVariants.h5} className="category-titles">
+                Contributors
+              </Content>
+              <Content component={ContentVariants.p}>{skillFormData.name}</Content>
+              <Content component={ContentVariants.p}>{skillFormData.email}</Content>
+            </div>
+          </div>
+        </article>
 
-      {/* File Path Information */}
-      <p>
-        <strong>File Path Information</strong>
-      </p>
-      <p>
-        <i>File Path:</i> {skillFormData.filePath}
-      </p>
+        {/* Skill Information */}
+        <article>
+          <div className="info-wrapper">
+            <Content component={ContentVariants.p} className="submission-titles">
+              Skill Information
+            </Content>
+            <Content component={ContentVariants.p} className="submission-subtitles">
+              Brief information about the Skill and the directory path for the QnA and Attribution files.
+            </Content>
+          </div>
 
-      {/* Seed Examples */}
-      <p>
-        <strong>Seed Examples</strong>
-      </p>
-      {skillFormData.seedExamples.map((seedExample, index) => (
-        <div key={index}>
-          <p>
-            <strong>Seed Examples {index + 1}</strong>
-          </p>
-          <p>
-            <i>Context:</i> {seedExample.context}
-          </p>
-          <p>
-            <i>Question {index + 1}:</i> {seedExample.question}
-          </p>
-          <p>
-            <i>Answer {index + 1}:</i> {seedExample.answer}
-          </p>
-        </div>
-      ))}
+          <Content component={ContentVariants.h5} className="category-titles">
+            Submission summary
+          </Content>
+          <Content component={ContentVariants.p}>{skillFormData.submissionSummary}</Content>
 
-      {/* Attribution Information */}
-      {isGithubMode && (
-        <div>
-          <p>
-            <strong>Attribution Information</strong>
-          </p>
-          <p>
-            <i>Title of Work:</i> {skillFormData.titleWork}
-          </p>
-          <p>
-            <i>License of Work:</i> {skillFormData.licenseWork}
-          </p>
-          <p>
-            <i>Creators:</i> {skillFormData.creators}
-          </p>
-        </div>
-      )}
-    </div>
+          <Content component={ContentVariants.h5} className="category-titles">
+            Directory path
+          </Content>
+          <Content component={ContentVariants.p}>{skillFormData.filePath}</Content>
+        </article>
+
+        {/* Seed Examples */}
+        <article>
+          <div className="info-wrapper">
+            <Content component={ContentVariants.p}>Seed Examples</Content>
+            <Content component={ContentVariants.p} className="submission-subtitles">
+              Data that will be used to start teaching your model.
+            </Content>
+          </div>
+
+          {skillFormData.seedExamples?.map((seedExample, index) => (
+            <Accordion asDefinitionList={false} className="accordion-wrapper" key={`seed-${index}`}>
+              <AccordionItem isExpanded={!!expanded[index]} key={`accordion-item-${index}`}>
+                <AccordionToggle onClick={() => onToggle(index)} id={`seed-example-toggle-${index}`} className="accordion-toggle-item">
+                  Sample {index + 1}
+                </AccordionToggle>
+                <AccordionContent id={`seed-example-content-${index}`}>
+                  <div className="accordion-content">
+                    <Content component={ContentVariants.h5} className="seed-category-titles">
+                      Context:
+                    </Content>{' '}
+                    {seedExample.context}
+                  </div>
+                  <div className="accordion-content">
+                    <Content component={ContentVariants.h5} className="seed-category-titles">
+                      Question:
+                    </Content>{' '}
+                    {seedExample.question}
+                  </div>
+                  <div className="accordion-content">
+                    <Content component={ContentVariants.h5} className="seed-category-titles">
+                      Answer:
+                    </Content>{' '}
+                    {seedExample.answer}
+                  </div>
+                </AccordionContent>
+              </AccordionItem>
+            </Accordion>
+          ))}
+        </article>
+
+        <article className="info-wrapper">
+          {/* Attribution Information */}
+          {isGithubMode && (
+            <div>
+              <Content component={ContentVariants.p}>
+                <strong>Attribution Information</strong>
+              </Content>
+              <Content component={ContentVariants.p}>
+                <i>Title of Work:</i> {skillFormData.titleWork}
+              </Content>
+              <Content component={ContentVariants.p}>
+                <i>License of Work:</i> {skillFormData.licenseWork}
+              </Content>
+              <Content component={ContentVariants.p}>
+                <i>Creators:</i> {skillFormData.creators}
+              </Content>
+            </div>
+          )}
+        </article>
+      </section>
+    </>
   );
 };
 
