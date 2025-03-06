7ffa350eddf14611e3e7d59817bd8f8a35e39ede
Kanchi Bhaskar <Kanchi.Bhaskar@ibm.com>
2025-03-06 16:53:20 +0530

    fix: push npm pretty and lint fixes
    Signed-off-by: Kanchi Bhaskar <Kanchi.Bhaskar@ibm.com>

--- src/app/login/devmodelogin.tsx
+++ src/app/login/devmodelogin.tsx
@@ -88,10 +88,18 @@ const DevModeLogin: React.FunctionComponent = () => {
             </Form>
           </div>
           <Content>
-          <Content component="p" className="account-create-text">
-            New to GitHub? <a href="https://github.com/signup?return_to=https%3A%2F%2Fgithub.com%2Finstructlab%2F&source=login"  style={{ textDecoration: 'underline', color:'#B9DAFC' }} target="_blank"> Create an account</a>
-          </Content>
-          <Content component="p" className="urls-text">
+            <Content component="p" className="account-create-text">
+              New to GitHub?{' '}
+              <a
+                href="https://github.com/signup?return_to=https%3A%2F%2Fgithub.com%2Finstructlab%2F&source=login"
+                style={{ textDecoration: 'underline', color: '#B9DAFC' }}
+                target="_blank"
+              >
+                {' '}
+                Create an account
+              </a>
+            </Content>
+            <Content component="p" className="urls-text">
               <a
                 href="https://github.com/instructlab/"
                 style={{ color: 'white', textDecoration: 'underline' }}
